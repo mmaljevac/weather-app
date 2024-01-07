f520b78d6e0d3865a9ea18d255545949b441cd66
Matija Maljevac <mmaljevac@neos.hr>
2024-01-07 19:52:43 +0100

    App basics

---
+++ screens/Home.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { globalStyles } from '../styles/globalStyles';
+import {
+  Button,
+  Keyboard,
+  Text,
+  TextInput,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
+
+export default function Home() {
+  const [cookies, setCookies] = useState(0);
+
+  const handleClick = () => {
+    setCookies(cookies + 1);
+    console.log(cookies + ' cookies');
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={globalStyles.container}>
+        <Text>Cookies: {cookies}</Text>
+        <TextInput placeholder="input"></TextInput>
+        <View style={globalStyles.buttonContainer}>
+          <Button title="+1" onPress={handleClick} />
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
