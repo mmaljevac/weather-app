445cd096918f233920cbaed8c0cdc1ea4afc9a07
Matija Maljevac <mmaljevac@neos.hr>
2024-02-03 23:11:28 +0100

    Add swipe to delete

--- components/LocationItem.js
+++ components/LocationItem.js
@@ -9,9 +9,12 @@ import {
   weatherImages,
   weatherImagesNight,
 } from '../constants/constants';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function LocationItem({ item }) {
-  const { setSavedLocations, setActiveLocation } = useAppContext();
+  const { savedLocations, setSavedLocations, setActiveLocation } =
+    useAppContext();
   const navigation = useNavigation();
 
   const [location, setLocation] = useState('');
@@ -39,48 +42,64 @@ export default function LocationItem({ item }) {
     navigation.goBack();
   };
 
-  const handleDelete = (item) => {
-    setSavedLocations((prevLocs) => {
-      return prevLocs.filter((loc) => loc.name != item.name);
-    });
+  const handleDelete = () => {
+    const updatedLocations = savedLocations.filter(
+      (savedItem) => savedItem.name + savedItem.country !== item.name + item.country
+    );
+    setSavedLocations(updatedLocations);
+  };
+
+  const rightSwipe = () => {
+    return (
+      <TouchableOpacity
+        style={styles.deleteBox}
+        onPress={handleDelete}
+      >
+        <Text style={{ color: 'red', fontWeight: 'bold' }}>Delete</Text>
+      </TouchableOpacity>
+    );
   };
 
   return (
     location && (
-      <TouchableOpacity
-        onPress={selectLocation}
-        style={[
-          styles.container,
-          {
-            backgroundColor: `rgba(${
-              location.current.is_day ? dayMode : nightMode
-            }, 1)`,
-          },
-        ]}
-      >
-        <View>
-          <Text
-            style={{
-              fontSize: 25,
-              padding: 5,
-              color: 'white',
-            }}
+      <GestureHandlerRootView>
+        <Swipeable renderRightActions={rightSwipe}>
+          <TouchableOpacity
+            onPress={selectLocation}
+            style={[
+              styles.container,
+              {
+                backgroundColor: `rgba(${
+                  location.current.is_day ? dayMode : nightMode
+                }, 1)`,
+              },
+            ]}
           >
-            {item.name}
-          </Text>
-          <Text style={styles.text}>
-            {location.current.temp_c}°C, {location.current.condition.text}
-          </Text>
-        </View>
-        <Image
-          source={
-            location.current.is_day
-              ? weatherImages[location.current.condition.text]
-              : weatherImagesNight[location.current.condition.text]
-          }
-          style={styles.img}
-        />
-      </TouchableOpacity>
+            <View>
+              <Text
+                style={{
+                  fontSize: 25,
+                  padding: 5,
+                  color: 'white',
+                }}
+              >
+                {item.name}
+              </Text>
+              <Text style={styles.text}>
+                {location.current.temp_c}°C, {location.current.condition.text}
+              </Text>
+            </View>
+            <Image
+              source={
+                location.current.is_day
+                  ? weatherImages[location.current.condition.text]
+                  : weatherImagesNight[location.current.condition.text]
+              }
+              style={styles.img}
+            />
+          </TouchableOpacity>
+        </Swipeable>
+      </GestureHandlerRootView>
     )
   );
 }
@@ -102,5 +121,10 @@ const styles = StyleSheet.create({
   img: {
     width: 55,
     height: 55,
+  },
+  deleteBox: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
   },
 });
